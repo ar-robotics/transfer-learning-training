f569eaae6da1d60f87da39ac2da9a8d0869984f0
Aditi <aditirdeshpande28@gmail.com>
2024-03-15 14:59:09 +0100

    made oop

--- Model_maker_training.py
+++ Model_maker_training.py
@@ -1,230 +1,218 @@
-import numpy as np
 import os
 import cv2
 import numpy as np
 import tensorflow as tf
-import contextlib
+
+# import contextlib
 from PIL import Image
 from matplotlib import pyplot as plt
-from tflite_model_maker.config import QuantizationConfig
+from absl import logging
+
+# from tflite_model_maker.config import QuantizationConfig
 from tflite_model_maker.config import ExportFormat
 from tflite_model_maker import model_spec
 from tflite_model_maker import object_detector
 
-from absl import logging
+# shows only errors
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+"""check tensorflow version"""
+tf.get_logger().setLevel("ERROR")
 
-COLORS = None
-
-
-def load_data(csv_file_path, images_dir):
-    """Loads data from the csv file.
-
-    Args:
-        csv_file_path: The file path to the csv file.
-        images_dir: The directory that contains images.
-
-    Returns:
-        A Dataloader containing the data of training, validation and test set.
-    """
-    train_data, test_data, validation_data = object_detector.DataLoader.from_csv(
-        csv_file_path, images_dir=images_dir
-    )
-    return train_data, test_data, validation_data
+logging.set_verbosity(logging.ERROR)
 
 
-def train_model(train_data, validation_data):
-    """Fine tunes the EfficientDet-Lite0 model on the given dataset.
+class DataLoader:
+    def __init__(self, csv_file_path, images_dir):
+        self.csv_file_path = csv_file_path
+        self.images_dir = images_dir
 
-    Args:
-        train_data: Training dataset, in the form of Dataloader.
-         validation_data: Validation dataset, in the form of Dataloader.
-
-    Returns:
-         A trained model.
-    """
-    spec = model_spec.get("efficientdet_lite0")
-    model = object_detector.create(
-        train_data,
-        model_spec=spec,
-        batch_size=4,
-        train_whole_model=False,
-        epochs=25,
-        validation_data=validation_data,
-    )
-    return model
-
-
-def preprocess_image(image_path, input_size):
-    """Preprocess the input image to feed to the TFLite model
-
-    Args:
-        image_path: path to the input image
-        input_size: the input size of the model
-    Returns:
-        preprocessed image in the format of (1, input_size, input_size, 3)
-        original image size
-    """
-    img = tf.io.read_file(image_path)
-    img = tf.io.decode_image(img, channels=3)
-    img = tf.image.convert_image_dtype(img, tf.uint8)
-    original_image = img
-    resized_img = tf.image.resize(img, input_size)
-    resized_img = resized_img[tf.newaxis, :]
-    resized_img = tf.cast(resized_img, dtype=tf.uint8)
-    return resized_img, original_image
+    def load_data(self):
+        """Loads data from the csv file.
+        Args:
+         csv_file_path: The file path to the csv file.
+         images_dir: The directory that contains images.
+        Returns:
+        A Dataloader containing the data of training, validation and test set
+        """
+        train_data, test_data, validation_data = (
+            object_detector.DataLoader.from_csv(  # noqa
+                self.csv_file_path, images_dir=self.images_dir
+            )
+        )
+        return train_data, test_data, validation_data
 
 
-def detect_objects(interpreter, image, threshold):
-    """Returns a list of detection results, each a dictionary of object info.
+class ModelTrainer:
+    def __init__(self, train_data, validation_data):
+        self.train_data = train_data
+        self.validation_data = validation_data
+        self.model = None
 
-    Args:
-        interpreter: tflite.Interpreter
-        image: A [1, height, width, 3] Tensor of type tf.uint8.threshold: a
-        floating point number
+    def train_model(self):
+        """Fine tunes the EfficientDet-Lite0 model on the given dataset.
+        Args:
+          train_data: Training dataset, in the form of Dataloader.
+          validation_data: Validation dataset, in the form of Dataloader.
+        Returns:
+          A trained model.
+        """
+        spec = model_spec.get("efficientdet_lite0")
+        self.model = object_detector.create(
+            self.train_data,
+            model_spec=spec,
+            batch_size=4,
+            train_whole_model=False,
+            epochs=25,
+            validation_data=self.validation_data,
+        )
+        return self.model
 
-    Returns:
-        a list of dicts
-    """
-    signature_fn = interpreter.get_signature_runner()
-
-    # Feed the input image to the model
-    output = signature_fn(images=image)
-
-    # Get all outputs from the model
-    count = int(np.squeeze(output["output_0"]))
-    scores = np.squeeze(output["output_1"])
-    classes = np.squeeze(output["output_2"])
-    boxes = np.squeeze(output["output_3"])
-
-    results = []
-    for i in range(count):
-        if scores[i] >= threshold:
-            result = {
-                "bounding_box": boxes[i],
-                "class_id": classes[i],
-                "score": scores[i],
-            }
-            results.append(result)
-    return results
+    def evaluate_and_export(self, test_data):
+        """Evaluates the model and exports it to the export directory.
+        Args:
+          test_data: Test dataset, in the form of Dataloader.
+        """
+        print(self.model.evaluate(test_data))
+        self.model.export(export_dir=".")
+        self.model.export(
+            export_dir=".",
+            export_format=[ExportFormat.TFLITE, ExportFormat.LABEL],  # noqa
+        )
+        self.model.export(
+            export_dir=".", tflite_filename="People_Detection.tflite"
+        )  # noqa
 
 
-def run_odt_and_draw_results(image_path, interpreter, threshold=0.5):
-    """Run object detection on the input image and draw the detection results
+csv_file_path = "people_updated_labels.csv"
+images_dir = "/mnt/c/Users/aditi/Downloads/archive/images/"
 
-    Args:
-        image_path: path to the input image
-        interpreter: tflite.Interpreter
-        threshold: a floating point number
-    """
-    # Load the input shape required by the model
-    _, input_height, input_width, _ = interpreter.get_input_details()[0]["shape"]
+data_loader = DataLoader(csv_file_path, images_dir)
+train_data, test_data, validation_data = data_loader.load_data()
 
-    # Load the input image and preprocess it
-    preprocessed_image, original_image = preprocess_image(
-        image_path, (input_height, input_width)
-    )
 
-    # Run object detection on the input image
-    results = detect_objects(interpreter, preprocessed_image, threshold=threshold)
+class ObjectDetector:
+    def __init__(self, model, model_path, classes):
+        """Creates an ObjectDetector instance.
+        Args:
+            model_path: The file path to the TFLite model.
+            classes: A list of class labels in the order of the model output.
+            The first element should be the class label of index 0.
+        """
+        # Define a list of colors for visualization
+        self.colors = np.random.randint(
+            0, 255, size=(len(classes), 3), dtype=np.uint8
+        )  # noqa
+        self.model_path = model_path
+        self.model = model
+        self.classes = classes
+        self.interpreter = tf.lite.Interpreter(model_path=self.model_path)
+        self.interpreter.allocate_tensors()
 
-    # Plot the detection results on the input image
-    original_image_np = original_image.numpy().astype(np.uint8)
-    for obj in results:
-        # Convert the object bounding box from relative coordinates to absolute
-        # coordinates based on the original image resolution
-        ymin, xmin, ymax, xmax = obj["bounding_box"]
-        xmin = int(xmin * original_image_np.shape[1])
-        xmax = int(xmax * original_image_np.shape[1])
-        ymin = int(ymin * original_image_np.shape[0])
-        ymax = int(ymax * original_image_np.shape[0])
+    def preprocess_image(self, image_path, input_size):
+        return
+        # Implementation remains the same as the function above
 
-        # Find the class index of the current object
-        class_id = int(obj["class_id"])
+    def detect_objects(self, image, threshold=0.5):
+        return
+        # Implementation remains the same as the function above
 
-        # Draw the bounding box and label on the image
-        color = [int(c) for c in COLORS[class_id]]
-        cv2.rectangle(original_image_np, (xmin, ymin), (xmax, ymax), color, 2)
-        # Make adjustments to make the label visible for all objects
-        y = ymin - 15 if ymin - 15 > 15 else ymin + 15
-        label = "{}: {:.0f}%".format(classes[class_id], obj["score"] * 100)
-        cv2.putText(
-            original_image_np, label, (xmin, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2
+    def run_odt_and_draw_results(self, image_path, interpreter, threshold=0.5):
+        """Run object detection on the input image and draw the results.
+        Args:
+            image_path: The file path to the input image.
+            interpreter: The TensorFlow Lite interpreter.
+            threshold: The minimum confidence score for detected objects.
+        Returns:
+            A NumPy array of the input image with the detection results.
+        """
+        # Load the input shape required by the model
+        _, input_h, input_w, _ = interpreter.get_input_details()[0]["shape"]
+
+        # Load the input image and preprocess it
+        preprocessed_image, original_image = self.preprocess_image(
+            image_path, (input_h, input_w)
         )
 
-    # Return the final image
-    original_uint8 = original_image_np.astype(np.uint8)
-    return original_uint8
+        # Run object detection on the input image
+        results = self.detect_objects(
+            interpreter, preprocessed_image, threshold=threshold
+        )
+
+        # Plot the detection results on the input image
+        original_image_np = original_image.numpy().astype(np.uint8)
+        for obj in results:
+            # Convert the object bounding box from relative coordinates to
+            # absolute coordinates based on the original image resolution
+            ymin, xmin, ymax, xmax = obj["bounding_box"]
+            xmin = int(xmin * original_image_np.shape[1])
+            xmax = int(xmax * original_image_np.shape[1])
+            ymin = int(ymin * original_image_np.shape[0])
+            ymax = int(ymax * original_image_np.shape[0])
+
+            # Find the class index of the current object
+            class_id = int(obj["class_id"])
+
+            # Draw the bounding box and label on the image
+            color = [int(c) for c in self.colors[class_id]]
+            cv2.rectangle(
+                original_image_np, (xmin, ymin), (xmax, ymax), color, 2
+            )  # noqa
+            # Make adjustments to make the label visible for all objects
+            y = ymin - 15 if ymin - 15 > 15 else ymin + 15
+            label = "{}: {:.0f}%".format(
+                self.classes[class_id], obj["score"] * 100
+            )  # noqa
+
+            cv2.putText(
+                original_image_np,
+                label,
+                (xmin, y),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.5,
+                color,
+                2,
+            )
+
+        # Return the final image
+        original_uint8 = original_image_np.astype(np.uint8)
+        return original_uint8
 
 
-if __name__ == "__main__":
-    # shows only errors
-    os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
-    """check tensorflow version"""
-    assert tf.__version__.startswith("2")
+model_trainer = ModelTrainer(train_data, validation_data)
+model = model_trainer.train_model()
+model_trainer.evaluate_and_export(test_data)
 
-    tf.get_logger().setLevel("ERROR")
 
-    logging.set_verbosity(logging.ERROR)
-    csv_file_path = "csv_files/people_updated_labels.csv"
-    images_dir = "images_zipped/"
+model_path = "People_Detection.tflite"
 
-    train_data, test_data, validation_data = load_data(csv_file_path, images_dir)
 
-    model = train_model(train_data, validation_data)
-    print(model.evaluate(test_data))
+local_image_path = "OIDv4_ToolKit/OID/test_all/0a80b03afcf13297.jpg"
+detection_threshold = 0.3
 
-    # Redirects the stdout to a file
-    with open("out.txt", "w") as f:
-        with contextlib.redirect_stdout(f):
-            # This will print the evaluation results to 'out.txt' instead of the standard output
-            print(model.evaluate(test_data))
+im = Image.open(local_image_path)
+im.thumbnail((512, 512), Image.LANCZOS)
+im.save("saved", "JPEG")
 
-    model.export(export_dir=".")  # Exports to the export directory
-    model.export(
-        export_dir=".", export_format=[ExportFormat.TFLITE, ExportFormat.LABEL]
-    )
-    model.export(
-        export_dir=".", tflite_filename="People_Detection_2.tflite"
-    )  # , quantization_config=config)
+# Load the TFLite model
+interpreter = tf.lite.Interpreter(model_path=model_path)
+interpreter.allocate_tensors()
+# Load the labels into a list
+classes = ["???"] * model.model_spec.config.num_classes
+label_map = model.model_spec.config.label_map
+for label_id, label_name in label_map.as_dict().items():
+    classes[label_id - 1] = label_name
 
-    print(model.evaluate_tflite("People_Detection_2.tflite", test_data))
+object_d = ObjectDetector(model, model_path, classes)
 
-    # Redirects the stdout to a file
-    with open("out.txt", "w") as f:
-        with contextlib.redirect_stdout(f):
-            # This will print the evaluation results to 'out.txt' instead of the standard output
-            print(model.evaluate_tflite("People_Detection_2.tflite", test_data))
 
-    model_path = "People_Detection_2.tflite"
+# Run inference and draw det result on the local copy of the og image
+detection_result_image = object_d.run_odt_and_draw_results(
+    local_image_path, interpreter, threshold=detection_threshold
+)
 
-    # Load the labels into a list
-    classes = ["???"] * model.model_spec.config.num_classes
-    label_map = model.model_spec.config.label_map
-    for label_id, label_name in label_map.as_dict().items():
-        classes[label_id - 1] = label_name
-
-    # Define a list of colors for visualization
-    COLORS = np.random.randint(0, 255, size=(len(classes), 3), dtype=np.uint8)
-
-    local_image_path = "0a80b03afcf13297.jpg"
-    detection_threshold = 0.3
-
-    im = Image.open(local_image_path)
-    im.thumbnail((512, 512), Image.LANCZOS)
-    im.save("saved", "JPEG")
-
-    # Load the TFLite model
-    interpreter = tf.lite.Interpreter(model_path=model_path)
-    interpreter.allocate_tensors()
-
-    # Run inference and draw detection result on the local copy of the original file
-    detection_result_image = run_odt_and_draw_results(
-        local_image_path, interpreter, threshold=detection_threshold
-    )
-
-    # Show the detection result
-    img2 = Image.fromarray(detection_result_image)
-    plt.figure(figsize=(10, 10))
-    plt.imshow(detection_result_image)
-    plt.axis("off")  # Don't show axes for images
-    plt.show()
+# Show the detection result
+img2 = Image.fromarray(detection_result_image)
+plt.figure(figsize=(10, 10))
+plt.imshow(detection_result_image)
+plt.axis("off")  # Don't show axes for images
+plt.show()
--- test_with_camera.py
+++ test_with_camera.py
@@ -1,125 +1,201 @@
 import cv2
 import numpy as np
 import tensorflow as tf
+from pymongo import MongoClient
 
 
-labels = []
+# Connect to MongoDB
+client = MongoClient("mongodb://localhost:27017/")
+db = client["mongodbVSCodePlaygroundDB"]
+
+# Initialize video capture
+cap = cv2.VideoCapture(0)
 
 
-def make_boxes(num_detections, detection_boxes, detection_classes, detection_scores):
-    """Draw the boxes on the frame and label them
-
-    Args:
-        num_detections: Number of detections
-        detection_boxes: Bounding box coordinates
-        detection_classes: Class indices
-        detection_scores: Confidence scores
-
-    Returns:
-        Frame with bounding boxes and labels"""
-    for i in range(num_detections):
-        if detection_scores[i] > 0.4:  # Confidence threshold
-            ymin, xmin, ymax, xmax = detection_boxes[i]
-            (left, right, top, bottom) = (
-                xmin * frame.shape[1],
-                xmax * frame.shape[1],
-                ymin * frame.shape[0],
-                ymax * frame.shape[0],
-            )
-            cv2.rectangle(
-                frame, (int(left), int(top)), (int(right), int(bottom)), (0, 255, 0), 2
-            )
-            # Draw label
-            object_name = labels[int(classes[i])]  # Retrieve the class name
-            label = "%s" % (object_name)  # Example: 'cat: 72%'
-            labelSize, baseLine = cv2.getTextSize(
-                label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2
-            )  # Get font size
-            top = max(top, labelSize[1])
-            cv2.rectangle(
-                frame,
-                (int(left), int(top - round(1.5 * labelSize[1]))),
-                (int(left + round(1.5 * labelSize[0])), int(top + baseLine)),
-                (255, 255, 255),
-                cv2.FILLED,
-            )
-            cv2.putText(
-                frame,
-                label,
-                (int(left), int(top)),
-                cv2.FONT_HERSHEY_SIMPLEX,
-                0.7,
-                (0, 0, 0),
-                2,
-            )
-    return frame
-
-
-# Loop over frames from the video file stream
-def interpret(frame, height, width, interpreter, input_details, output_details):
-    """Interpret the frame and make predictions
-
-    Args:
-        frame: Input frame
-        height: Frame height
-        width: Frame width
-        interpreter: TFLite interpreter
-        input_details: Input details
-        output_details: Output details
-
-    Returns:
-        num_detections: Number of detections
-        scores: Confidence scores
-        boxes: Bounding box coordinates
-        classes: Class indices
-    """
-    input_frame = cv2.resize(frame, (width, height))
-    input_frame = np.expand_dims(input_frame, axis=0).astype(np.uint8)
-    interpreter.set_tensor(input_details[0]["index"], input_frame)
-    interpreter.invoke()
-    num_detections = int(interpreter.get_tensor(output_details[2]["index"])[0])
-    scores = interpreter.get_tensor(output_details[0]["index"])[0]
-    boxes = interpreter.get_tensor(output_details[1]["index"])[0]
-    classes = interpreter.get_tensor(output_details[3]["index"])[0]
-    return num_detections, scores, boxes, classes
-
-
-if __name__ == "__main__":
-    # Load TFLite model and allocate tensors
-    interpreter = tf.lite.Interpreter(
-        model_path="tflite_models/people_detection_2.tflite"
-    )
-    interpreter.allocate_tensors()
-
-    # Get model details
-    input_details = interpreter.get_input_details()
-    output_details = interpreter.get_output_details()
-
-    height = int(input_details[0]["shape"][1])
-    width = int(input_details[0]["shape"][2])
-
-    with open("labels.txt", "r") as f:
+def load_labels(path):
+    with open(path, "r") as f:
         labels = [line.strip() for line in f.readlines()]
+    return labels
 
-    # Initialize video capture
-    cap = cv2.VideoCapture(0)
 
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
+labels = load_labels("labels-ssd.txt")
 
-        num_detections, scores, boxes, classes = interpret(
-            frame, height, width, interpreter, input_details, output_details
+
+class Interpreter:
+    def __init__(self, model_path):
+        self.model_path = model_path
+        self.interpreter = tf.lite.Interpreter(model_path=self.model_path)
+        self.interpreter.allocate_tensors()
+
+    def get_interpreter(self):
+        return self.interpreter
+
+    def get_details(self):
+        input_details = self.interpreter.get_input_details()
+        output_details = self.interpreter.get_output_details()
+        height = int(input_details[0]["shape"][1])
+        width = int(input_details[0]["shape"][2])
+        return input_details, output_details, height, width
+
+
+class Detection:
+    def __init__(
+        self, frame, height, width, interpreter, input_detail, output_detail
+    ):  # noqa
+        self.frame = frame
+        self.height = height
+        self.width = width
+        self.interpreter = interpreter
+        self.input_detail = input_detail
+        self.output_detail = output_detail
+        self.scores = None
+        self.boxes = {}
+        self.classes = None
+        self.num_detections = None
+
+    def interpret(self):
+        """Interpret the frame and make predictions
+
+        Args:
+            frame: Input frame
+            height: Frame height
+            width: Frame width
+            interpreter: TFLite interpreter
+            input_details: Input details
+            output_details: Output details
+
+        Returns:
+            num_detections: Number of detections
+            scores: Confidence scores
+            boxes: Bounding box coordinates
+            classes: Class indices
+        """
+        input_frame = cv2.resize(self.frame, (self.width, self.height))
+        input_frame = np.expand_dims(input_frame, axis=0).astype(np.uint8)
+        self.interpreter.set_tensor(self.input_detail[0]["index"], input_frame)
+        self.interpreter.invoke()
+        self.num_detections = int(
+            self.interpreter.get_tensor(self.output_detail[2]["index"])[0]
         )
-        make_boxes(num_detections, boxes, classes, scores)
-        # Display the resulting frame
-        cv2.imshow("Object Detection", frame)
+        self.scores = self.interpreter.get_tensor(  # noqa
+            self.output_detail[0]["index"]
+        )[0]
+        self.boxes = self.interpreter.get_tensor(  # noqa
+            self.output_detail[1]["index"]
+        )[0]
+        self.classes = self.interpreter.get_tensor(  # noqa
+            self.output_detail[3]["index"]
+        )[0]
 
-        # Break loop with 'q'
-        if cv2.waitKey(1) & 0xFF == ord("q"):
-            break
+    def make_boxes(self):
+        """Draw the boxes on the frame and label them
 
-    # Release capture
-    cap.release()
-    cv2.destroyAllWindows()
+        Args:
+            num_detections: Number of detections
+            detection_boxes: Bounding box coordinates
+            detection_classes: Class indices
+            detection_scores: Confidence scores
+
+        Returns:
+            Frame with bounding boxes and labels"""
+        for i in range(self.num_detections):
+            if self.scores[i] > 0.4:  # Confidence threshold
+                ymin, xmin, ymax, xmax = self.boxes[i]
+                (left, right, top, bottom) = (
+                    xmin * frame.shape[1],
+                    xmax * frame.shape[1],
+                    ymin * frame.shape[0],
+                    ymax * frame.shape[0],
+                )
+                cv2.rectangle(
+                    frame,
+                    (int(left), int(top)),
+                    (int(right), int(bottom)),
+                    (0, 255, 0),
+                    2,  # noqa
+                )
+                # Retrieve the class name
+                object_name = labels[int(self.classes[i])]
+                query = {"type": object_name}
+                items_info = employment.find_one(query)
+                all_info_dict = {}
+
+                # Loop through all key-value pairs in the items_info document
+                for key, value in items_info.items():
+                    # Save each key-value pair into the dictionary
+                    all_info_dict[key] = value
+                print(all_info_dict)
+                # Draw label
+                # label = "%s" % (object_name)  # Example: 'cat: 72%'
+                label_line_1 = f"{object_name}"  # , Age: {age}"
+                # label_line_2 = (
+                #     f"Date of Employment:
+                # {date_of_employment.strftime('%Y-%m-%d')}"
+                # )
+
+                (text_width, text_height), baseLine = cv2.getTextSize(
+                    label_line_1, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2
+                )
+                # Set the starting Y position for the text
+                text_y_start = int(top - round(1.5 * text_height))
+
+                # Draw the first line of text
+                cv2.putText(
+                    frame,
+                    label_line_1,
+                    (int(left), text_y_start),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    0.7,
+                    (0, 0, 0),
+                    2,
+                )
+                """Adjust Y position for the second line
+                based on the height of the first line
+                """
+                text_y_start += text_height + baseLine
+
+                # Draw the second line of text
+                cv2.putText(
+                    frame,
+                    # label_line_2,
+                    (int(left), text_y_start),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    0.7,
+                    (0, 0, 0),
+                    2,
+                )
+
+        return frame
+
+
+employment = db["classes"]
+
+
+interpreter = Interpreter(
+    "C:/Users/aditi/Documents/Bachelor_p/Object_Detection-pi/pre-trained model/tflite-models/ssd_mobilenet.tflite"
+)
+input_details, output_details, height, width = interpreter.get_details()
+interpreter = interpreter.get_interpreter()
+
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+    # Detect objects
+    detection_obj = Detection(
+        frame, height, width, interpreter, input_details, output_details
+    )
+    detection_obj.interpret()
+    frame = detection_obj.make_boxes()
+
+    # Display the resulting frame
+    cv2.imshow("Object Detection", frame)
+
+    # Break loop with 'q'
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+# Release capture
+cap.release()
+cv2.destroyAllWindows()
