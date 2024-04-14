2a84386a9abc435cbead0a46f27dd11ef8c6f8ee
Aditi <aditirdeshpande28@gmail.com>
2024-04-14 19:27:38 +0200

    yolo-tflite conversion

--- yolo-tflite.py
+++ yolo-tflite.py
@@ -1,10 +1,6 @@
-import os
-
-os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
 import cv2
 import numpy as np
 import tensorflow as tf
-
 
 # Initialize video capture
 cap = cv2.VideoCapture(0)
@@ -38,6 +34,80 @@ class Interpreter:
         return input_details, output_details, height, width
 
 
+class Process:
+    def __init__(
+        self, frame, height, width, interpreter, input_details, output_details
+    ):
+        self.frame = frame
+        self.height = height
+        self.width = width
+        self.interpreter = interpreter
+        self.input_details = input_details
+        self.output_details = output_details
+
+    def draw_detections(self, frame, box, score, class_id):
+        left, top, right, bottom = box
+        cv2.rectangle(frame, (left, top), (right, bottom), (10, 255, 0), 2)
+        cv2.putText(
+            frame,
+            "{}: {:.2f}".format("Person", score),
+            (left, top - 5),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.5,
+            (10, 255, 0),
+            2,
+        )
+
+    def preprocess(self):
+        frame = cv2.resize(self.frame, (self.width, self.height))
+        frame = np.expand_dims(frame, axis=0).astype(np.float32)
+        return frame
+
+    def postprocess(self, frame, output):
+        output = np.transpose(np.squeeze(output[0]))
+        rows = output.shape[0]
+        boxes = []
+        scores = []
+        class_ids = []
+
+        for i in range(rows):
+
+            classes_scores = output[i][4:]
+            max_score = np.amax(classes_scores)
+
+            if max_score >= 0.9:
+                class_id = np.argmax(classes_scores)
+
+                x, y, w, h = output[i][0], output[i][1], output[i][2], output[i][3]
+                left = int((x - w / 2) * self.width)
+                top = int((y - h / 2) * self.height)
+                right = int((x + w / 2) * self.width)
+                bottom = int((y + h / 2) * self.height)
+
+                class_ids.append(class_id)
+                scores.append(max_score)
+                boxes.append([left, top, right, bottom])
+
+        indices = cv2.dnn.NMSBoxes(boxes, scores, 0.9, 0.4)
+
+        for i in indices:
+            box = boxes[i]
+            score = scores[i]
+            class_id = class_ids[i]
+
+            self.draw_detections(frame, box, score, class_id)
+        return frame
+
+    def main(self):
+
+        frame = self.preprocess()
+        self.interpreter.set_tensor(self.input_details[0]["index"], frame)
+        self.interpreter.invoke()
+        output_data = self.interpreter.get_tensor(self.output_details[0]["index"])
+        frame = self.postprocess(self.frame, output_data)
+        return frame
+
+
 class Detection:
     def __init__(self, height, width, interpreter, input_detail, output_detail):  # noqa
         self.h = height
@@ -46,27 +116,7 @@ class Detection:
         self.input_details = input_detail
         self.output_details = output_detail
 
-    def nms(self, boxes, scores, iou_threshold):
-
-        return
-
     def interpret(self, frame):
-        """Interpret the frame and make predictions
-
-        Args:
-            frame: Input frame
-            height: Frame height
-            width: Frame width
-            interpreter: TFLite interpreter
-            input_details: Input details
-            output_details: Output details
-
-        Returns:
-            num_detections: Number of detections
-            scores: Confidence scores
-            boxes: Bounding box coordinates
-            classes: Class indices
-        """
         input_frame = cv2.resize(frame, (self.w, self.h))
         input_frame = np.expand_dims(input_frame, axis=0).astype(np.float32)
         self.interpreter.set_tensor(self.input_details[0]["index"], input_frame)  # noqa
@@ -91,31 +141,10 @@ class Detection:
         output_data = output_data[0]  # x(1, 25200, 7) to x(25200, 7)
         boxes_tensor = output_data[..., :4]  # boxes  [25200, 4]
         scores_tensor = output_data[..., 4:5]  # confidences  [25200, 1]
-        classes_tensor = output_data[..., 5:]  # classes  [25200, 3]
+        classes = output_data[..., 5:]  # classes  [25200, 3]
 
-        boxes = tf.reshape(
-            boxes_tensor, [1, -1, 1, 4]
-        )  # Reshape to [batch_size, num_boxes, num_classes, 4]
-
-        scores = tf.reshape(
-            scores_tensor, [1, -1, 1]
-        )  # Reshape to [batch_size, num_boxes, num_classes]
-
-        # Apply combined_non_max_suppression
-        nmsed_boxes, nmsed_scores, nmsed_classes, valid_detections = (
-            tf.image.combined_non_max_suppression(
-                boxes=boxes,
-                scores=scores,
-                max_output_size_per_class=10,
-                max_total_size=10,
-                iou_threshold=0.5,
-                score_threshold=0.5,
-            )
-        )
-        boxes = nmsed_boxes.numpy()
-        scores = nmsed_scores.numpy()
-
-        classes = self.classFilter(output_data[..., 5:])  # get classes
+        boxes = np.squeeze(boxes_tensor)  # [25200, 4]
+        scores = np.squeeze(scores_tensor)  # [25200, 1]
         x, y, w, h = boxes[..., 0], boxes[..., 1], boxes[..., 2], boxes[..., 3]  # xywh
         xyxy = [x - w / 2, y - h / 2, x + w / 2, y + h / 2]  # xywh to xyxy   [4, 25200]
 
@@ -128,7 +157,6 @@ class Detection:
     def make_boxes_2(self, frame, xyxy, classes, scores):
         for i in range(len(scores)):
             if (scores[i] > 0.9) and (scores[i] <= 1.0):
-                print(xyxy[0], xyxy[1], xyxy[2], xyxy[3])
                 xmin = int(max(1, (xyxy[0][i] * self.w)))
                 ymin = int(max(1, (xyxy[1][i] * self.h)))
                 xmax = int(min(self.h, (xyxy[2][i] * self.w)))
@@ -153,27 +181,22 @@ interpreter = Interpreter(
 )
 input_details, output_details, height, width = interpreter.get_details()
 interpreter = interpreter.get_interpreter()
-
-
 while True:
     ret, frame = cap.read()
     if not ret:
         break
     # Detect objects
-    detection_obj = Detection(
-        height, width, interpreter, input_details, output_details
-    )  # noqa
-
-    frame = detection_obj.interpret(frame)
-
-    # frame = detection_obj.make_boxes()
-    # Display the resulting frame
-    # cv2.imshow("Object Detection", frame)
-
+    # detection_obj = Detection(
+    #     height, width, interpreter, input_details, output_details
+    # )  # noqa
+    # frame = detection_obj.interpret(frame)
+    detection_obj = Process(
+        frame, height, width, interpreter, input_details, output_details
+    )
+    frame = detection_obj.main()
+    cv2.imshow("Object Detection", frame)
     # Break loop with 'q'
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
 # Release capture
-cap.release()
-cv2.destroyAllWindows()
