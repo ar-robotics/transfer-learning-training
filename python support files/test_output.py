5b355cdd20b335b8f3c30400041d3b8d66b39647
Aditi <aditirdeshpande28@gmail.com>
2024-04-24 09:07:56 +0200

    trying to plot

--- python support files/test_output.py
+++ python support files/test_output.py
@@ -19,18 +19,3 @@ for output_detail in output_details:
     print(i, output_detail)
     i += 1
 
-# image_data = Image.open("sha.png").resize((640, 640))
-# image_data = np.array(image_data).astype(np.float32)
-# image_data = np.expand_dims(image_data, axis=0)
-# interpreter.set_tensor(input_details[0]["index"], image_data)
-# interpreter.invoke()
-# output_data = interpreter.get_tensor(output_details[0]["index"])
-# output_data = output_data[..., [1, 0, 3, 2]]
-# output_data[..., 0:4] = output_data[..., 0:4] * 640
-# output_data[..., 5] = np.argmax(output_data[..., 5], axis=-1)
-
-# # Round the confidence score to 2 decimal places
-# output_data[..., 4] = np.round(output_data[..., 4], decimals=2)
-
-# # Print the output shape and data
-# print(output_data)
--- python support files/tf-to-tflite.py
+++ python support files/tf-to-tflite.py
@@ -1,11 +1,24 @@
-import tensorflow as tf
-import numpy as np
+# import tensorflow as tf
+# import numpy as np
 import cv2
 
 
-saved_model_path = "C:/Users/aditi/Documents/Bachelor_p/weights/best_saved_model/"
-converter = tf.lite.TFLiteConverter.from_saved_model(saved_model_path)
-tflite_model = converter.convert()
+# saved_model_path = "C:/Users/aditi/Documents/Bachelor_p/weights/best_saved_model/"
+# converter = tf.lite.TFLiteConverter.from_saved_model(saved_model_path)
+# tflite_model = converter.convert()
 
-with open("people_yolov5_float32.tflite", "wb") as f:
-    f.write(tflite_model)
+# with open("people_yolov5_float32.tflite", "wb") as f:
+#     f.write(tflite_model)
+
+
+# Assuming 'frame' is your loaded or captured frame
+frame = cv2.imread('t.jpg') 
+(h,w) = frame.shape[:2]
+end_effector_point = (w//2, h//2)  # Example point
+# Draw the circle
+cv2.circle(frame, end_effector_point, radius=2, color=(0, 0, 255), thickness=-1)
+
+# Display the image
+cv2.imshow('Frame with Circle', frame)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
