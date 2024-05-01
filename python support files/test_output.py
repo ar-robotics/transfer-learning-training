0c1a454a39c685116a2c3d120f99f0505e71490b
Aditi <aditirdeshpande28@gmail.com>
2024-05-01 21:34:14 +0200

    merge

--- python support files/test_output.py
+++ python support files/test_output.py
@@ -1,31 +1,21 @@
-# import tensorflow as tf
-# from PIL import Image
-# import numpy as np
 
-# # Load TFLite model and allocate tensors
-# interpreter = tf.lite.Interpreter(
-#     model_path="tflite_models/ssd_mobilenet.tflite"
-# )  # noqa
-# interpreter.allocate_tensors()
+import tensorflow as tf
+from PIL import Image
+import numpy as np
 
-# # Get model details
-# input_details = interpreter.get_input_details()
-# output_details = interpreter.get_output_details()
+# Load TFLite model and allocate tensors
+interpreter = tf.lite.Interpreter(
+    model_path="tflite_models/ssd_mobilenet.tflite"
+)  # noqa
+interpreter.allocate_tensors()
 
-# i = 0
-# # print(output_details)
-# for output_detail in output_details:
+# Get model details
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
 
-#     print(i, output_detail)
-#     i += 1
+i = 0
+# print(output_details)
+for output_detail in output_details:
 
-def check_file_encoding(file_path):
-    try:
-        with open(file_path, encoding='utf-8') as file:
-            file.read()
-        print("No encoding issues detected.")
-    except UnicodeDecodeError as e:
-        print(f"Encoding issue detected: {e}")
-
-# Replace 'your_notebook.ipynb' with the path to your notebook
-# check_file_encoding('docs/notebooks/Custom_training_bolt_detection.ipynb')
+    print(i, output_detail)
+    i += 1
